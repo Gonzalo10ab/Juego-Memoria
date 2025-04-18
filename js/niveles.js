47a3d119ec08528cbb6fc4045abe774502b53e01
Gonzalo10ab <gmt0009@alu.medac.es>
2025-04-18 15:02:15 +0200

    Corregido dimensiones del tablero

--- js/niveles.js
+++ js/niveles.js
@@ -2,7 +2,7 @@
 const niveles = {
     1: {
         filas: 2,             // Número de filas del tablero
-        columnas: 2,          // Número de columnas del tablero
+        columnas: 3,          // Número de columnas del tablero
         powerups: false,      // No se permite usar powerups en este nivel
         joker: false,         // No hay carta Joker disponible
         oscuridad: false,     // No se activa el efecto de oscuridad
@@ -18,7 +18,7 @@ const niveles = {
     },
     3: {
         filas: 4,
-        columnas: 4,
+        columnas: 6,
         powerups: true,
         joker: false,
         oscuridad: false,
@@ -26,14 +26,14 @@ const niveles = {
     },
     4: {
         filas: 4,
-        columnas: 5,
+        columnas: 6,
         powerups: false,
         joker: true,
         oscuridad: false,
         contrarreloj: false
     },
     5: {
-        filas: 6,
+        filas: 4,
         columnas: 6,
         powerups: true,
         joker: false,
@@ -41,7 +41,7 @@ const niveles = {
         contrarreloj: true
     },
     6: {
-        filas: 8,
+        filas: 6,
         columnas: 6,
         powerups: true,
         joker: true,
